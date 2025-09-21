ec61e5187c88fa5f53be0afa258dbb3f54651197
Winki <163889519+Winki372@users.noreply.github.com>
2025-09-21 17:23:58 +0700

    Create Dosen.java

---
+++ Modul 2/2.5 Pekerjaan Rumah/no 2/Dosen.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+        
+public class Dosen {
+    String nama;
+    int nik;
+    String pendidikan;
+    Date tglLahir = new Date();
+
+    void tampilkanNama(){
+        System.out.println("Nama Dosen adalah: " + nama);
+    }
+    void tampilkanAlamat(){
+        System.out.println("Alamat Dosen : " );
+    }
+    void tampilkantglLahir(){
+        System.out.println("Tanggal Lahir Dosen adalah: " + tglLahir);
+    }
+    void tampilkanNik(){
+        System.out.println("NIK dosen adalah: " + nik);
+    }
+}
