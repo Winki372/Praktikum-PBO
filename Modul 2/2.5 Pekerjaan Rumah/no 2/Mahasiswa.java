0740732e23b29a785783c7df76885cf480127707
Winki <163889519+Winki372@users.noreply.github.com>
2025-09-21 17:24:27 +0700

    Create Mahasiswa.java

---
+++ Modul 2/2.5 Pekerjaan Rumah/no 2/Mahasiswa.java
@@ -0,0 +1,19 @@
+public class Mahasiswa {
+    String nama;
+    String nim;
+    String alamat;
+    int semester;
+
+    void tampilkanNama(){
+        System.out.println("Nama Mahasiswa: " + nama);
+    }
+    void tampilkanNim(){
+        System.out.println("NIM : " + nim);
+    }
+    void tampilkanAlamat(){
+        System.out.println("Alamat Mahasiswa: " + alamat);
+    }
+    void tampilkanSemester(){
+        System.out.println("Semester Mahasiswa: " + semester);
+    }
+}
